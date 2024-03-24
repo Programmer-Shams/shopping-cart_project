60630c5b8481b85a437c67cc7e8070ec2a3756ec
Programmer-Shams <ssdeen313@gmail.com>
2024-03-24 19:28:58 +0100

    fix an error that occur while deploying

--- src/components/ProductBox.tsx
+++ src/components/ProductBox.tsx
@@ -21,6 +21,7 @@ const ProductBox = ({ FruitName, FruitPrice, fruitImg, id }: FruitProps) => {
   }
 
   const { increaseCartQuantity } = useShoppingCart();
+  
 
   return (
     <div className="bg-bg_color2 p-4 w-[250px] rounded-xl shadow-md hover:scale-[1.05] transition-transform duration-500">
